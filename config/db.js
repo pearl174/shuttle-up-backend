f3aa093e7460b31fd82e40a16ddd64b861f65fbf
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-07 00:20:11 +0530

    Add database configuration file. Install mongoose.

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// establishing external database connection to mongodb
+// using mongoose
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1); // Exit process if DB connection fails
+  }
+};
+
+module.exports = connectDB;
