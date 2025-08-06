138f1058ca63e1f97b991062d9921a1388c0a44b
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-07 00:04:44 +0530

    Add package.json, .gitignore, and app.js basic setup.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
