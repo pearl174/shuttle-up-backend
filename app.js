f318df5ed73c8979f10bbe1ee610bb57a982030e
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-09 16:03:23 +0530

    Add auth route mention in app.js

--- app.js
+++ app.js
@@ -15,5 +15,8 @@ app.use(cors({
     credentials: true
 }));
 
+// Routes
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
