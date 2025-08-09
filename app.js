59872d7c0d201563c7abb8fd8696dc7ac11edc89
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-09 14:56:40 +0530

    Add json parser and set cors policy with origin URI.

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 connectDB();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+const authRoutes = require("./routes/authRoutes.js");
+
+app.use(express.json());
+app.use(cors({
+    origin: "https://localhost:3000",
+    credentials: true
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
