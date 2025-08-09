496d55bf3a17fc6d06e41b1d8e86b3b00ec0f1c3
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-09 16:23:06 +0530

    Add register route with checks and signup callback. Signup to be defined.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const { check } = require("express-validator");
+const { signup, login } = require("../controller/authController.js");
+
+const router = express.Router();
+
+// @route   POST /api/auth/register
+// @desc    Register a new user
+// @access  Public
+router.post(
+    "/register",
+    [
+        check("username", "Username is required").not().isEmpty(),
+        check("email", "Include a valid email").isEmail(),
+        check("password", "Password must be 6 or more chars").isLength({ min: 6 })
+    ],
+    signup
+);
