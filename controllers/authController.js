aef8808212e1d13ee7444196444a06e0e05831be
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-09 17:37:01 +0530

    Add login function in authController. Return error array if validation error. Otherwise check for username and password and return JWT token to frontend.

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,6 +36,36 @@ exports.signup = async(req, res) => {
         res.status(201).json({ msg: "Signup successful! Please login" });
     } catch (err) {
         console.error(err.message);
-        res.status(500).send("Server Error");
+        res.status(500).json({ msg: "Internal Server Error" });
+    }
+}
+
+exports.login = async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { username, password } = req.body;
+    
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+        // console.log(user._id); // ObjectId("64f23e9bdbe13a45c5f7c0aa")
+        // console.log(user.id);  // "64f23e9bdbe13a45c5f7c0aa"  (string)
+
+        const payload = { user: { id: user.id } };
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+        res.status(200).json({ token });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ msg: "Internal Server Error" });
     }
 }
