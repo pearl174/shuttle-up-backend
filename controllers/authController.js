0ee4ca7ef126cb01dfdd6c68a8d94ffa1bd1b318
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-09 17:10:16 +0530

    Add signup function. Check for errors, create new user and profile.

---
+++ controllers/authController.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const User = require("../models/User.js");
+const Profile = require("../models/Profile.js");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+exports.signup = async(req, res) => {
+    const errors = validationResult(req); // the req body has errors stored for each one triggered in the authRoute
+    // as the checks were in the middleware of .post. get added to the request body and can be gathered here
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() }); // bad request
+    }
+    const { username, email, password } = req.body;
+
+    try {
+        const [existingEmail, existingUsername] = await Promise.all([
+            User.findOne({ email }),
+            User.findOne({ username })
+        ]);
+
+        if (existingEmail || existingUsername) {
+            return res.status(400).json({ msg: "Email or Username already exists" });
+        }
+
+        // Create new user
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+
+        const profile = new Profile({ user: user._id, username });  
+        await profile.save();
+
+        res.status(201).json({ msg: "Signup successful! Please login" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+}
